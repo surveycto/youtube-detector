48426c7e546e0083463052c9a23f930a6bc77c48
Max S. Haberman <max@surveycto.com>
2022-07-18 15:49:20 -0400

    Added proper error for when "player" is not added

--- source/script.js
+++ source/script.js
@@ -20,7 +20,8 @@ var playerContainer = document.querySelector('#player')
 var loadingNode = document.querySelector('.loader')
 
 if (playerContainer == null) {
-  playerContainer.innerHTML = 'Error: No element with id "player". Please see the <a href="https://github.com/surveycto/youtube-detector#important-adding-the-video" target="_blank">documentation</a> to learn how to add the video element.'
+  loadingNode.style.display = 'none'
+  document.querySelector('.error').style.display = ''
 }
 
 var currentAnswer // Stores the currently selected value, but will not set until minSeconds has been met
